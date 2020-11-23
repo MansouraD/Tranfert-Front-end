97151a05852cb4d79245121b8f576aae69b45861
mansoura D <mariama1.diouf@gmail.com>
2020-11-23 15:56:01 +0000

    Transfert-partie-front

---
+++ src/app/emetteur/emetteur.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-emetteur',
+  templateUrl: './emetteur.component.html',
+  styleUrls: ['./emetteur.component.css']
+})
+export class EmetteurComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/recepteur/recepteur.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recepteur',
+  templateUrl: './recepteur.component.html',
+  styleUrls: ['./recepteur.component.css']
+})
+export class RecepteurComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
